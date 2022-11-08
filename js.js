22cf7f761399dcbced5661f6ca9e14dfe7966b0f
abdou <abdennour.oussaid9@gmail.com>
2022-11-08 08:46:41 -0800

    working on the js file

--- js.js
+++ js.js
@@ -2,14 +2,16 @@ let rate=document.getElementById("rateBook");
 let radioBtn=document.querySelectorAll("[type='radio']");
 let yesRadioBtn=radioBtn[0];
 let noRadioBtn=radioBtn[1];
+let closeForm=document.getElementById("closeForm");
+let popUpField=document.getElementById("popUp");
+let overlay=document.getElementById("overlay");
+let main =document.getElementsByTagName('main');
 
 function checkRadioButton(){
     let haveRead=document.getElementById("yes").checked;
     if(haveRead){
-        console.log(`yes is being selected`);
         rate.classList.remove('hide');
     }else{
-        console.log(`no is being selected`);
         rate.classList.add('hide');
     }
 }
@@ -18,4 +20,75 @@ yesRadioBtn.onclick=()=>{
 }
 noRadioBtn.onclick=()=>{
     checkRadioButton();
+}
+closeForm.onclick=()=>{
+    popUpField.remove();
+    overlay.classList.remove("active");
+};
+
+// function book(title,author,pages,isRead,starsNbr){
+//     this.title;
+//     this.author;
+//     this.pages;
+//     this.isRead;
+//     this.starsNbr;
+// }
+
+let form=document.getElementById("form");
+form.addEventListener("submit",(e)=>{
+    e.preventDefault();
+    const newForm= new FormData(form);
+    const obj= Object.fromEntries(newForm)
+    console.log(obj);
+});
+
+let addToLibraryBtn= document.getElementById("add-to-library-btn");
+
+addToLibraryBtn.onclick=()=>{
+    createCard();
+}
+
+function createCard(){
+    let cardContainer=document.createElement('div');
+    cardContainer.setAttribute('class','card-container');
+    main[0].appendChild(cardContainer);
+
+    let theCard=document.createElement('div');
+    theCard.setAttribute('class','the-card');
+    cardContainer.appendChild(theCard);
+
+    let theFront=document.createElement("div")
+    theFront.setAttribute("class","theFront");
+    theCard.appendChild(theFront);
+
+    let img=document.createElement('img');
+    img.setAttribute('src','');
+    img.setAttribute('alt','Book image');
+    theFront.appendChild(img);
+    let theBack=document.createElement("div");
+    theBack.setAttribute("class","theBack");
+    theCard.appendChild(theBack);
+
+    let bookInfo=document.createElement("div");
+    bookInfo.setAttribute('class','book-info');
+    theBack.appendChild(bookInfo);
+    for (let i = 1; i<=3 ;i++) {
+        if (i==1) {
+            let abbr=document.createElement('abbr');
+            abbr.setAttribute('title','BOOK Title');
+            bookInfo.appendChild(abbr);
+        }else if(i==2){
+            let abbr=document.createElement('abbr');
+            abbr.setAttribute('title','BOOK Author');
+            bookInfo.appendChild(abbr);
+    }else{
+        let abbr=document.createElement('abbr');
+            abbr.setAttribute('title','Number Of Pages');
+            bookInfo.appendChild(abbr);
+    }
+    }
+    let button=document.createElement('button');
+    button.setAttribute('type','button');
+    theBack.appendChild(button);
+    button.textContent='Delete';
 }
